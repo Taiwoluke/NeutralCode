522a8f32f56ddbdaf5778f442e5b75b96787d96b
Taiwo Oladamola <taiwoluke@gmail.com>
2025-07-21 19:02:25 +0100

    Frontend Setup

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+const Nav = () => {
+
+  return (
+    <nav>
+        <img src="" alt="logo" />
+
+        <ul>
+            <li><a href="">The_Neutralcode_Experience</a></li>
+            <li><a href="">Stacks</a></li>
+            <li><a href="">The_Team</a></li>
+            <li><a href="">Chit_Chat</a></li>
+            <li><a href="">Studentship</a></li>
+            <li><a href="">Mentorshipship</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
