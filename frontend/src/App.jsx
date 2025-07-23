701e30d8444c4a40a12a74fc51df90af39c3bdc0
Taiwo Oladamola <taiwoluke@gmail.com>
2025-07-23 16:35:11 +0100

    setup the react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
     <>
-      <Nav/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
       
     </>
   )
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Nav = () => {
 
   return (
-    <nav>
+    <nav className='bg-black min-w-10 h-20'>
         <img src="" alt="logo" />
 
-        <ul className='text-center bg-blue'>
-            <li><a href="">The_Neutralcode_Experience</a></li>
-            <li><a href="">Stacks</a></li>
-            <li><a href="">The_Team</a></li>
-            <li><a href="">Chit_Chat</a></li>
-            <li><a href="">Studentship</a></li>
-            <li><a href="">Mentorshipship</a></li>
+        <ul className='flex flex-row justify-center gap-5.5 m-auto'>
+            <li className='text-white'><a href="">TheNeutralcodeExperience</a></li>
+            <li className='text-white'><a href="">Stacks</a></li>
+            <li className='text-white'><a href="">TheTeam</a></li>
+            <li className='text-white'><a href="">ChitChat</a></li>
+            <li className='text-white'><a href="">Studentship</a></li>
+            <li className='text-white'><a href="">Mentorship</a></li>
         </ul>
     </nav>
   )
